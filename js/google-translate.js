417ece1b4beef76036ccb171817b5b36d3f5be1f
get-web <get-web@protonmail.com>
2019-02-01 02:50:11 +0200

    Оптимизация

--- js/google-translate.js
+++ js/google-translate.js
@@ -3,36 +3,40 @@ const googleTranslateConfig = {
 };
 
 function TranslateInit() {
-    // Инициализируем виджет с языком по умолчанию
-    new google.translate.TranslateElement({
-        pageLanguage: googleTranslateConfig.lang,
-    });
 
     // Если куки нет, то передаем дефолтный язык
     let lang = ($.cookie('googtrans') != undefined && $.cookie('googtrans') != "null") ? $.cookie('googtrans') : googleTranslateConfig.lang;
     // Находим флаг с выбранным языком и добавляем к нему активный класс
     $('[data-google-lang="' + lang.substr(-2) + '"]').addClass('language__img_active');
 
+    if (lang.substr(-2) == googleTranslateConfig.lang) {
+        // Если язык по умолчанию, совпадает с языком на который переводим
+        // То очищаем куки
+        TranslateClearCookie();
+    }
+
+    // Инициализируем виджет с языком по умолчанию
+    new google.translate.TranslateElement({
+        pageLanguage: googleTranslateConfig.lang,
+    });
+
 
     // Вешаем событие  клик на флаги
     $('[data-google-lang]').click(function () {
-        if ($(this).attr("data-google-lang") == googleTranslateConfig.lang) {
-            // Если язык по которому кликунули соответствует дефолтному
-            // То очищаем куки
-            $.cookie('googtrans', null);
-            $.cookie("googtrans", null, {
-                domain: "." + document.domain,
-            });
-        } else {
-            // Если язык по которому кликунули не соответствует дефолтному
-            // то записываем его в куки
-            $.cookie('googtrans', "/" + googleTranslateConfig.lang + "/" + $(this).attr("data-google-lang"));
-            $.cookie("googtrans", "/" + googleTranslateConfig.lang + "/" + $(this).attr("data-google-lang"), {
-                domain: "." + document.domain,
-            });
-        }
+        // Записываем куки /язык_который_переводим/язык_на_который_переводим
+        $.cookie('googtrans', "/auto/" + $(this).attr("data-google-lang"));
+        $.cookie("googtrans", "/auto/" + $(this).attr("data-google-lang"), {
+            domain: "." + document.domain,
+        });
         // Перезагружаем страницу
         window.location.reload();
+    });
+}
+
+function TranslateClearCookie() {
+    $.cookie('googtrans', null);
+    $.cookie("googtrans", null, {
+        domain: "." + document.domain,
     });
 }
 
