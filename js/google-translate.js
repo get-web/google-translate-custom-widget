edb142f98780db0626f46cc170aa5dc34c60539d
get-web <get-web@protonmail.com>
2019-02-01 13:02:27 +0200

    update

--- js/google-translate.js
+++ js/google-translate.js
@@ -4,12 +4,12 @@ const googleTranslateConfig = {
 
 function TranslateInit() {
 
-    // Если куки нет, то передаем дефолтный язык
-    let lang = ($.cookie('googtrans') != undefined && $.cookie('googtrans') != "null") ? $.cookie('googtrans') : googleTranslateConfig.lang;
-    // Находим флаг с выбранным языком и добавляем к нему активный класс
-    $('[data-google-lang="' + lang.substr(-2) + '"]').addClass('language__img_active');
 
-    if (lang.substr(-2) == googleTranslateConfig.lang) {
+    let code = TranslateGetCode();
+    // Находим флаг с выбранным языком для перевода и добавляем к нему активный класс
+    $('[data-google-lang="' + code + '"]').addClass('language__img_active');
+
+    if (code == googleTranslateConfig.lang) {
         // Если язык по умолчанию, совпадает с языком на который переводим
         // То очищаем куки
         TranslateClearCookie();
@@ -20,17 +20,18 @@ function TranslateInit() {
         pageLanguage: googleTranslateConfig.lang,
     });
 
-
     // Вешаем событие  клик на флаги
     $('[data-google-lang]').click(function () {
-        // Записываем куки /язык_который_переводим/язык_на_который_переводим
-        $.cookie('googtrans', "/auto/" + $(this).attr("data-google-lang"));
-        $.cookie("googtrans", "/auto/" + $(this).attr("data-google-lang"), {
-            domain: "." + document.domain,
-        });
+        TranslateSetCookie($(this).attr("data-google-lang"))
         // Перезагружаем страницу
         window.location.reload();
     });
+}
+
+function TranslateGetCode() {
+    // Если куки нет, то передаем дефолтный язык
+    let lang = ($.cookie('googtrans') != undefined && $.cookie('googtrans') != "null") ? $.cookie('googtrans') : googleTranslateConfig.lang;
+    return lang.substr(-2);
 }
 
 function TranslateClearCookie() {
@@ -40,4 +41,10 @@ function TranslateClearCookie() {
     });
 }
 
-
+function TranslateSetCookie(code) {
+    // Записываем куки /язык_который_переводим/язык_на_который_переводим
+    $.cookie('googtrans', "/auto/" + code);
+    $.cookie("googtrans", "/auto/" + code, {
+        domain: "." + document.domain,
+    });
+}
