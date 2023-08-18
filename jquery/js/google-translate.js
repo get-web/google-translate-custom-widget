e38a38bf36e81f5aa280366262dce10698bdd072
Vitalii P <admgetweb@gmail.com>
2023-08-18 22:20:34 +0300

    Upgrade to 1.0.6

--- jquery/js/google-translate.js
+++ jquery/js/google-translate.js
@@ -1,20 +1,30 @@
 /*!***************************************************
- * google-translate.js v1.0.5
+ * google-translate.js v1.0.6
  * https://Get-Web.Site/
  * author: Vitalii P.
  *****************************************************/
+
+
+/* Вы можете перенести данный конфиг в head своего сайта, чтобы динамически конфигурировать значения при помощи данных из CMS */
+/* You can transfer this config to the head of your site to dynamically configure values using data from the CMS */
+
 
 const googleTranslateConfig = {
 	/* Original language */
 	lang: "ru",
 
+	/* Если хотите подписаться на событие "FinishTranslate" (Момент когда скрипт закончил перевод), расскоментируйте и добавьте любое проверочное слово на оригинальном языке */
+	/* If you want to subscribe to the "FinishTranslate" event (The moment when the script finished translating), uncomment and add any test word in the original language */
+	// testWord: "Язык",
+
 	/* The language we translate into on the first visit*/
 	/* Язык, на который переводим при первом посещении */
-	/* langFirstVisit: 'en', */
+	// langFirstVisit: 'en',
 
 	/* Если скрипт не работает или работает неправильно, раскомментируйте и укажите основной домен в свойстве domain */
 	/* If the script does not work or does not work correctly, uncomment and specify the main domain in the domain property */
-	/* domain: "Get-Web.Site" */
+	// domain: "Get-Web.Site"
+
 };
 
 $(function () {
@@ -45,6 +55,9 @@ function TranslateInit(config) {
 		/* If the default language is the same as the language we are translating into, then we clear the cookies */
 		TranslateCookieHandler(null, config.domain);
 	}
+
+	if (config.testWord) TranslateMutationObserver(config.testWord, code == config.lang);
+
 
 	/* Инициализируем виджет с языком по умолчанию */
 	/* Initialize the widget with the default language */
@@ -106,4 +119,33 @@ function TranslateHtmlHandler(code) {
 	/* Получаем язык на который переводим и производим необходимые манипуляции с DOM */
 	/* We get the language to which we translate and produce the necessary manipulations with DOM */
 	$('[data-google-lang="' + code + '"]').addClass("language__img_active");
+}
+
+
+function TranslateMutationObserver(word, isOrigin) {
+
+	if (isOrigin) {
+		document.dispatchEvent(new CustomEvent("FinishTranslate"));
+	} else {
+
+		/* Создаем скрытый блок в который добавляем тестовое слово на оригинальном языке. Это позволит нам отследить момент когда сайт будет переведен и вызвать событие "FinishTranslate"  */
+		/* Creating a hidden block in which we add a test word in the original language. This will allow us to track the moment when the site is translated and trigger the "FinishTranslate" event  */
+
+		let div = document.createElement('div');
+		div.id = 'googleTranslateTestWord';
+		div.innerHTML = word;
+		div.style.display = 'none';
+		document.body.prepend(div);
+
+		let observer = new MutationObserver(() => {
+			document.dispatchEvent(new CustomEvent("FinishTranslate"));
+			observer.disconnect();
+		});
+
+		observer.observe(div, {
+			childList: false,
+			subtree: true,
+			characterDataOldValue: true
+		});
+	}
 }
